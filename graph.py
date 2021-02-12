83df86d60814b5085cd2bb57e8f9bfbcfbe5fbc1
Krisztian Lachata <krisztian.lachata@ovoenergy.com>
2021-02-12 08:33:55 +0000

    add problematic coloring for topic

--- graph.py
+++ graph.py
@@ -2,6 +2,7 @@ import logging
 from graphviz import Digraph
 from dataclasses import dataclass, field
 import uuid
+from collections import Counter
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -23,18 +24,19 @@ class Node:
 def generate(acls, self_link_generator, generate_topic_download_link, get_static_resource, **kwargs):
     dot = Digraph(comment='Kafka Topic Graph')
 
-    topic_nodes = set()
+    topics = set()
     username_nodes = set()
+    topic_nodes = set()
     edges = set()
 
     for acl in acls:
-        topic_nodes.add(Node(acl['topic'],
-                       {'shape': 'rectangle',
-                        'label': f"<{topic_label(acl['topic'], self_link_generator, generate_topic_download_link, get_static_resource)}>"}))
-        username_nodes.add(Node(acl['username'], {'shape': 'ellipse', 'style': 'filled', 'fillcolor': 'lawngreen',
-                                         'label': acl['username'],
-                                         'URL': self_link_generator(acl['username']),
-                                         'tooltip': 'Zoom'}))
+        topics.add(acl['topic'])
+        username_nodes.add(Node(acl['username'], {'shape': 'ellipse',
+                                                  # 'style': 'filled',
+                                                  # 'fillcolor': 'lawngreen',
+                                                  'label': acl['username'],
+                                                  'URL': self_link_generator(acl['username']),
+                                                  'tooltip': 'Zoom'}))
         if acl['permission'] == 'read':
             edges.add(Edge(acl['topic'], acl['username'], {'color': 'blue'}))
         elif acl['permission'] == 'write':
@@ -42,6 +44,21 @@ def generate(acls, self_link_generator, generate_topic_download_link, get_static
         else:
             edges.add(Edge(acl['topic'], acl['username'], {'color': 'blue'}))
             edges.add(Edge(acl['username'], acl['topic'], {'color': 'red'}))
+
+    duplicated = get_duplicated_topics(topics)
+
+    # add color to problematic topics
+    for topic in topics:
+        if is_problematic_topic(topic, duplicated):
+            topic_nodes.add(Node(topic,
+                           {'shape': 'rectangle',
+                            'style': 'filled',
+                            'fillcolor': 'yellow',
+                            'label': f"<{topic_label(topic, self_link_generator, generate_topic_download_link, get_static_resource)}>"}))
+        else:
+            topic_nodes.add(Node(topic,
+                           {'shape': 'rectangle',
+                            'label': f"<{topic_label(topic, self_link_generator, generate_topic_download_link, get_static_resource)}>"}))
 
     for node in username_nodes.union(topic_nodes):
         dot.node(node.name, **node.style)
@@ -54,6 +71,23 @@ def generate(acls, self_link_generator, generate_topic_download_link, get_static
     logger.info(f'File rendered to {rendered}')
     with open(rendered, "rb") as file:
         return rendered, file.read()
+
+
+def is_problematic_topic(topic, duplicated):
+    if topic_without_version(topic) in duplicated:
+        return True
+    else:
+        return False
+
+
+def topic_without_version(topic):
+    return topic.split('_v')[0]
+
+
+def get_duplicated_topics(topics):
+    topics_without_version = [topic_without_version(topic) for topic in topics]
+    c = Counter(topics_without_version)
+    return [e for e in c if c[e] > 1]
 
 
 def topic_label(topic, self_link_generator, generate_topic_download_link, get_static_resource):
